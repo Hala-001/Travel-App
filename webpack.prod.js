e33f58238ead2dffb16a26712668ae68e1bc501d
Hala-001 <hala143375@gmail.com>
2020-12-09 14:46:04 +0300

    service-worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,9 +4,14 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 module.exports = {
     mode: 'production',
     entry: './src/client/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     optimization: {
         minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
         },
@@ -29,7 +34,8 @@ plugins: [
                 template: "./src/client/views/index.html",
                 filename: "./index.html",
             }),
-            new MiniCssExtractPlugin({ filename: "[name].css" })
+            new MiniCssExtractPlugin({ filename: "[name].css" }),
+            new WorkboxPlugin.GenerateSW()
         ]
 }
 
